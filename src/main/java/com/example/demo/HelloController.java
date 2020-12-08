93c79840db411b9baaf351d21c62eaed933941ba
agadient <agadient@mit.edu>
2020-12-08 14:15:07 -0700

    add source

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,51 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/math/pi")
+    @GetMapping("/")
     public String helloWorld() {
+        return "Hello from Spring!";
+    }
+
+    @GetMapping("/math/pi")
+    public String pi() {
         return "3.141592653589793!";
     }
+
+    @GetMapping("/math/calculate")
+    public Map calculate(@RequestParam Map querystring) {
+        String operation = "";
+        if (querystring.containsKey("operation")) {
+            operation = (String) querystring.get("operation");
+        }
+        Integer x = 1;
+        Integer y = 1;
+        if (querystring.containsKey("x")) {
+            x = Integer.parseInt((String) querystring.get("x"));
+        }
+
+        if (querystring.containsKey("y")) {
+            y = Integer.parseInt((String) querystring.get("y"));
+        }
+        String result = "";
+        if (operation.equals("subtract")) {
+            result = String.format("%d - %d = %d", x, y, x-y);
+        } else if (operation.equals("multiply")) {
+            result = String.format("%d * %d = %d", x, y, x*y);
+        } else if (operation.equals("divide")) {
+            result = String.format("%d / %d = %d", x, y, x/y);
+        } else {
+            result = String.format("%d + %d = %d", x, y, x+y);
+        }
+        return querystring;
+    }
+
 
 }
--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -1,28 +1,68 @@
 package com.example.demo;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HelloController.class)
 public class HelloControllerTest {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@Test
-	public void testIndexEndpoint() throws Exception {
-		this.mvc.perform(get("/").accept(MediaType.TEXT_PLAIN))
-				.andExpect(status().isOk())
-				.andExpect(content().string("3.141592653589793!"));
-	}
+    @Test
+    public void testIndexEndpoint() throws Exception {
+        this.mvc.perform(get("/").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello from Spring!"));
+    }
+
+    @Test
+    public void testPi() throws Exception {
+        this.mvc.perform(get("/math/pi").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("3.141592653589793!"));
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 6 = 10"));
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 - 6 = -2"));
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 * 6 = 24"));
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=5").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 / 5 = 6"));
+    }
+
+    @Test
+    public void testEmpty() throws Exception {
+        this.mvc.perform(get("/math/calculate?x=30&y=5").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 + 5 = 35"));
+    }
 
 }
